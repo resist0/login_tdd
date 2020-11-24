42ca017b3cbc4dd2dc6341b59c37a893ad9d7853
Muller <rm80491@fiap.com.br>
2020-11-23 22:31:16 -0300

    ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,8 +20,8 @@ class HttpAdapter {
       'content-type': 'application/json ',
       'accept': 'application/json',
     };
-
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -31,6 +31,7 @@ void main() {
   HttpAdapter sut;
   ClientSpy client;
   String url;
+
   setUp(() {
     client = ClientSpy();
     sut = HttpAdapter(client);
@@ -49,6 +50,15 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}',
+      ));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
       ));
     });
   });
