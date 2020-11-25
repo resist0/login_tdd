8be42db3c662d1ac6394092ba389072e7ff86f7c
Muller <rm80491@fiap.com.br>
2020-11-25 04:20:07 -0300

    add LoginPage layout

--- lib/ui/components/app.dart
+++ lib/ui/components/app.dart
@@ -5,9 +5,57 @@ import '../pages/pages.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+    final primaryColorDark = Color.fromRGBO(96, 9, 39, 1);
+    final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+
     return MaterialApp(
       title: '4Dev',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // Colors Theme
+        primaryColor: primaryColor,
+        primaryColorDark: primaryColorDark,
+        primaryColorLight: primaryColorLight,
+        accentColor: primaryColor,
+        backgroundColor: Colors.white,
+
+        // Text Theme
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: primaryColorDark,
+          ),
+        ),
+
+        // Input Theme
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: primaryColorLight,
+            ),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: primaryColor,
+            ),
+          ),
+          alignLabelWithHint: true,
+        ),
+
+        // Button Theme
+        buttonTheme: ButtonThemeData(
+          colorScheme: ColorScheme.light(primary: primaryColor),
+          buttonColor: primaryColor,
+          splashColor: primaryColorLight,
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          textTheme: ButtonTextTheme.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
       home: LoginPage(),
     );
   }
