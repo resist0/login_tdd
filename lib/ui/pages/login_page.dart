d2c0fd5026c506e729bd50b04eeab28c142923ad
Muller <rm80491@fiap.com.br>
2020-11-25 19:43:29 -0300

    ensure LoginPage loads with correct initial state

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -24,6 +24,7 @@ class LoginPage extends StatelessWidget {
                           Icons.email,
                           color: Theme.of(context).primaryColorLight,
                         ),
+                        
                       ),
                       keyboardType: TextInputType.emailAddress,
                     ),
@@ -41,7 +42,7 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                     RaisedButton(
-                      onPressed: () {},
+                      onPressed: null,
                       child: Text('Entrar'.toUpperCase()),
                     ),
                     FlatButton.icon(
