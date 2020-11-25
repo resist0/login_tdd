0a56c7c9fba0dd10a96aee8c662f5e7ac892fb9f
Muller <rm80491@fiap.com.br>
2020-11-24 21:08:59 -0300

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -3,9 +3,9 @@ import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordev/data/http/http.dart';
+
 import 'package:fordev/infra/http/http.dart';
-
-
 
 class ClientSpy extends Mock implements Client {}
 
@@ -83,5 +83,22 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400,body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
   });
 }
