ffcee407618428a3bc6c74f9bba76b5f4c04343f
Muller <rm80491@fiap.com.br>
2020-11-30 18:23:28 -0300

    ensure LoginPage presents error if email is invalid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -11,16 +13,22 @@ class LoginPresenterSpy extends Mock implements LoginPresenter{}
 void main() {
 
   LoginPresenter presenter;
+  StreamController<String> emailErrorController;
   
   Future<void> loadPage(WidgetTester tester) async {
     presenter = LoginPresenterSpy();
-    final loginPage = MaterialApp(home: LoginPage(presenter));
+    emailErrorController = StreamController<String>();
+    when(presenter.emailErrorStream).thenAnswer((_) => emailErrorController.stream);    final loginPage = MaterialApp(home: LoginPage(presenter));
     await tester.pumpWidget(loginPage);
   }
 
+  tearDown((){
+    emailErrorController.close();
+  });
+
+
   testWidgets('Should load with correct initial state', (WidgetTester tester) async {
     await loadPage(tester);
-
 
     final emailTextChildren = find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text));
     expect(
@@ -29,7 +37,6 @@ void main() {
       reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the hint text',
     );
 
-
     final passwordTextChildren = find.descendant(of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
     expect(
       passwordTextChildren,
@@ -37,11 +44,11 @@ void main() {
       reason: 'when a TextFormField has only one text child, means it has no errors, since one of the childs is always the hint text',
     );
 
-
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
 
   });
+
 
   testWidgets('Should call validate with correct values', (WidgetTester tester) async {
     await loadPage(tester);
@@ -54,6 +61,29 @@ void main() {
     await tester.enterText(find.bySemanticsLabel('Senha'), password);
     verify(presenter.validatePassword(password));
 
+  });
+
+
+  testWidgets('Should present error if email is invalid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add('any error');
+    await tester.pump();
+    expect(find.text('any error'), findsOneWidget);
+
+  });
+
+
+  testWidgets('Should present no error if email is valid', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    emailErrorController.add(null);
+    await tester.pump();
+
+    expect(
+      find.descendant(of: find.bySemanticsLabel('Email'), matching: find.byType(Text)),
+      findsOneWidget,
+    );
 
   });
 
