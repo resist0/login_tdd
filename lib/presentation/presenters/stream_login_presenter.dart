db0ab1f860317eb72b3eca52685489d910e007b5
Muller <rm80491@fiap.com.br>
2020-12-01 18:17:09 -0300

    ensure StreamLoginPresenter calls Validation on password changed

--- lib/presentation/presenters/stream_login_presenter.dart
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -12,16 +12,25 @@ class StreamLoginPresenter {
   final Validation validation;
   final _controller = StreamController<LoginState>.broadcast();
 
-  var _state = LoginState(); 
+  var _state = LoginState();
 
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError).distinct();
-  Stream<bool> get isFormValidStream => _controller.stream.map((state) => state.isFormValid).distinct();
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError).distinct();
+
+  Stream<bool> get isFormValidStream =>
+      _controller.stream.map((state) => state.isFormValid).distinct();
 
   StreamLoginPresenter({@required this.validation});
 
-  void validateEmail(String email){
+
+  void validateEmail(String email) {
     _state.emailError = validation.validate(field: 'email', value: email);
     _controller.add(_state);
   }
+
+  void validatePassword(String password) {
+    validation.validate(field: 'password', value: password);
+  }
+
 
 }
