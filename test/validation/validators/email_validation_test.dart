b61af7dda17dfe53a48ddce38769fda1f46394af
Muller <rm80491@fiap.com.br>
2020-12-03 20:12:19 -0300

    ensure EmailValidation returns null if email is empty

---
+++ test/validation/validators/email_validation_test.dart
@@ -0,0 +1,37 @@
+import 'package:test/test.dart';
+
+import 'package:fordev/validation/protocols/protocols.dart';
+
+class EmailValidation implements FieldValidation {
+  final String field;
+
+  EmailValidation(this.field);
+
+  String validate(String value) {
+    return null;
+  }
+  
+}
+
+void main() {
+
+  EmailValidation sut;
+
+  setUp((){
+    sut = EmailValidation('any_field');
+  });
+
+  test('Should return null if email is empty', () {
+    final error = sut.validate('');
+
+    expect(error, null);
+  });
+
+
+    test('Should return null if email is null', () {
+    final error = sut.validate(null);
+
+    expect(error, null);
+  });
+
+}
