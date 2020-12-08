cbcfb53a51a24a8a50becaad0c65013380f62a1a
Muller <rm80491@fiap.com.br>
2020-12-08 19:33:12 -0300

    move SUT creation to the setup method

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -23,12 +23,19 @@ class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
 void main() {
 
-  test('Should call save secure with correct values', () async {
-    final secureStorage = FlutterSecureStorageSpy();
-    final sut = LocalStorageAdapter(secureStorage: secureStorage);
-    final key = faker.guid.guid();
-    final value = faker.guid.guid();
+  LocalStorageAdapter sut;
+  FlutterSecureStorageSpy secureStorage;
+  String key;
+  String value;
 
+  setUp((){
+     secureStorage = FlutterSecureStorageSpy();
+     sut = LocalStorageAdapter(secureStorage: secureStorage);
+     key = faker.guid.guid();
+     value = faker.guid.guid();
+  });
+
+  test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
