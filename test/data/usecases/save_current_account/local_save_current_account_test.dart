7d8bd7496e021b305681bc82bfdb4b99c64c7c9a
Muller <rm80491@fiap.com.br>
2020-12-07 22:27:53 -0300

    ensure LocalSaveCurrentAccount throws UnexpectedError if SaveSecureCacheStorageSpy throws

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,4 +1,5 @@
 import 'package:faker/faker.dart';
+import 'package:fordev/domain/helpers/domain_error.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -10,8 +11,14 @@ class LocalSaveCurrentAccount implements SaveCurrentAccount {
   final SaveSecureCacheStorage saveSecureCacheStorage;
 
   LocalSaveCurrentAccount({@required this.saveSecureCacheStorage});
+
   Future<void> save(AccountEntity account) async {
-    await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    try {
+      await saveSecureCacheStorage.saveSecure(key: 'token', value: account.token);
+    } catch(error) {
+      throw DomainError.unexpected;
+    }
+    
   }
 }
 
@@ -23,14 +30,29 @@ class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCacheStorage {
 
 void main() {
 
-  test('Should call saveCacheStorage with correct values', () async {
+  test('Should call SaveSecureCacheStorage with correct values', () async {
     final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
     final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
     final account = AccountEntity(faker.guid.guid());
 
     await sut.save(account);
 
-    verify(saveSecureCacheStorage.saveSecure(key: 'token', value: 'account.token'));
+    verify(saveSecureCacheStorage.saveSecure(key: 'token', value: account.token));
+
+  });
+
+
+  
+  test('Should throw UnexpectedError if SaveSecureCacheStorage throws', () async {
+    final saveSecureCacheStorage = SaveSecureCacheStorageSpy();
+    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    final account = AccountEntity(faker.guid.guid());
+    when(saveSecureCacheStorage.saveSecure(key: anyNamed('key'), value: anyNamed('value')))
+    .thenThrow(Exception());
+
+    final future = sut.save(account);
+
+    expect(future, throwsA(DomainError.unexpected));
 
   });
 }
