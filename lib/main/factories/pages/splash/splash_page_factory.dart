9babd8bba5271bcdfebb6c977059b324a9e22768
Muller <rm80491@fiap.com.br>
2020-12-16 19:59:28 -0300

    add SplashPage factories

---
+++ lib/main/factories/pages/splash/splash_page_factory.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+
+import '../../../../ui/pages/pages.dart';
+import '../../factories.dart';
+
+Widget makeSplashPage() => SplashPage(presenter: makeGetxSplashPresenter());
