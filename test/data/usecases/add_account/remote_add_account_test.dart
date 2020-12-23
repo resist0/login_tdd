0369a33c51bd86e65a8747a711885c3c0230770b
Muller <rm80491@fiap.com.br>
2020-12-23 19:39:52 -0300

    Ensure RemoteAddAccount throws UnexpectedError on 200 with invalid data

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -110,4 +110,15 @@ void main() {
     expect(account.token, validData['accessToken']);
   });
 
+
+  test(
+      'Should throw UnexpectedError if HttpClient returns 200 with invalid data',
+      () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
