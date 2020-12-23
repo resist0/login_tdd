c5d50ed182dfced161e62a5c2cb7d93d0c52de54
Muller <rm80491@fiap.com.br>
2020-12-23 19:24:53 -0300

    Ensure RemoteAddAccount throws UnexpectedError on 400

--- lib/data/usecases/add_account/remote_add_account.dart
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -1,6 +1,6 @@
 import 'package:meta/meta.dart';
 
-import '../../../domain/entities/account_entity.dart';
+import '../../../domain/helpers/helpers.dart';
 import '../../../domain/usecases/usecases.dart';
 
 import '../../http/http.dart';
@@ -16,7 +16,11 @@ class RemoteAddAccount {
 
   Future<void> add(AddAccountParams params) async {
     final body = RemoteAddAccountParams.fromDomain(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
 
   }
 }
--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -3,6 +3,7 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
 
 import 'package:fordev/data/usecases/usecases.dart';
@@ -15,6 +16,16 @@ void main() {
   HttpClientSpy httpClient;
   String url;
   AddAccountParams params;
+
+  PostExpectation mockRequest() => when(httpClient.request(
+    url: anyNamed('url'),
+    method: anyNamed('method'),
+    body: anyNamed('body'))
+  );
+
+  void mockHttpError(HttpError error) {
+    mockRequest().thenThrow(error);
+  }
 
 
   setUp(() {
@@ -43,4 +54,14 @@ void main() {
           'passwordConfirmation': params.passwordConfirmation,
         }));
   });
+
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    mockHttpError(HttpError.badRequest);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
