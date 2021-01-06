7502203740b35306642dbb54daa2ed92fbc7507c
Muller <rm80491@fiap.com.br>
2021-01-05 22:05:07 -0300

    add SurveyEntity

---
+++ lib/domain/entities/survey_entity.dart
@@ -0,0 +1,17 @@
+import 'package:meta/meta.dart';
+
+class SurveyEntity {
+  final String id;
+  final String question;
+  final DateTime dateTime;
+  final bool didAnswer;
+
+
+  SurveyEntity({
+    @required this.id,
+    @required this.question,
+    @required this.dateTime,
+    @required this.didAnswer,
+  });
+
+}
