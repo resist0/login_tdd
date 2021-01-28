47cf4684290366aa776a427cd56cd12f3f52eee0
Muller <rm80491@fiap.com.br>
2021-01-28 18:37:29 -0300

    ensure RemoteLoadSurveys throws AccessDeniedError on 403

--- test/data/usecases/load_surveys/remote_load_surveys_test.dart
+++ test/data/usecases/load_surveys/remote_load_surveys_test.dart
@@ -23,8 +23,10 @@ class RemoteLoadSurveys {
       return httpResponse
           .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
           .toList();
-    } on HttpError {
-      throw DomainError.unexpected;
+    } on HttpError catch(error) {
+      throw error == HttpError.forbidden
+        ? DomainError.accessDenied
+        : DomainError.unexpected;
     }
   }
 }
@@ -123,6 +125,15 @@ void main() {
     final future = sut.load();
 
     expect(future, throwsA(DomainError.unexpected));
+  });
+
+
+  test('Should throw AccessDeniedError if HttpClient returns 403', () async {
+    mockHttpError(HttpError.forbidden);
+
+    final future = sut.load();
+
+    expect(future, throwsA(DomainError.accessDenied));
   });
 
 
