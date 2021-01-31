39f1a7cd4e01c9a2203032a32c37f1fd278f8b92
Muller <rm80491@fiap.com.br>
2021-01-31 01:44:31 -0300

    move GetxSurveysPresenter to its own files

--- test/presentation/presenters/getx_surveys_presenter_test.dart
+++ test/presentation/presenters/getx_surveys_presenter_test.dart
@@ -1,47 +1,15 @@
 import 'package:faker/faker.dart';
-import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 import 'package:test/test.dart';
-
-import 'package:fordev/ui/helpers/errors/errors.dart';
-import 'package:fordev/ui/pages/pages.dart';
 
 import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
 
-class GetxSurveysPresenter {
-  final LoadSurveys loadSurveys;
+import 'package:fordev/presentation/presenters/presenters.dart';
 
-  final _isLoading = true.obs;
-  final _surveys = Rx<List<SurveyViewModel>>();
-
-  Stream<bool> get isLoadingStream => _isLoading.stream;
-  Stream<List<SurveyViewModel>> get surveysStream => _surveys.stream;
-
-  GetxSurveysPresenter({@required this.loadSurveys});
-
-  Future<void> loadData() async {
-    try {
-      _isLoading.value = true;
-      final surveys = await loadSurveys.load();
-      _surveys.value = surveys
-          .map((survey) => SurveyViewModel(
-                id: survey.id,
-                question: survey.question,
-                date: DateFormat('dd MMM yyyy').format(survey.dateTime),
-                didAnswer: survey.didAnswer,
-              ))
-          .toList();
-    } on DomainError {
-      _surveys.subject.addError(UIError.unexpected.description);
-    } finally {
-      _isLoading.value = false;
-    }
-  }
-}
+import 'package:fordev/ui/helpers/helpers.dart';
+import 'package:fordev/ui/pages/pages.dart';
 
 class LoadSurveysSpy extends Mock implements LoadSurveys {}
 
@@ -49,6 +17,7 @@ void main() {
   GetxSurveysPresenter sut;
   LoadSurveysSpy loadSurveys;
   List<SurveyEntity> surveys;
+
   List<SurveyEntity> mockValidData() => [
         SurveyEntity(
           id: faker.guid.guid(),
@@ -85,8 +54,10 @@ void main() {
     verify(loadSurveys.load()).called(1);
   });
 
+
   test('Should emit correct events on success', () async {
     expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
     sut.surveysStream.listen(expectAsync1((surveys) => expect(surveys, [
           SurveyViewModel(
             id: surveys[0].id,
@@ -110,6 +81,7 @@ void main() {
     mockLoadSurveysError();
 
     expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
     sut.surveysStream.listen(null, onError: expectAsync1((error) => expect(error, UIError.unexpected.description)));
 
     await sut.loadData();
