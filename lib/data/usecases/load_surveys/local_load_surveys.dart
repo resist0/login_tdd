d7d197dbf9df3779f3cad7ff2976bcdbb6257f20
muller <rm80491@fiap.com.br>
2021-02-18 18:43:21 -0300

    make LocalLoadSurveys implements LoadSurveys

--- lib/data/usecases/load_surveys/local_load_surveys.dart
+++ lib/data/usecases/load_surveys/local_load_surveys.dart
@@ -2,11 +2,12 @@ import 'package:meta/meta.dart';
 
 import '../../../domain/entities/entities.dart';
 import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
 
 import '../../models/models.dart';
 import '../../cache/cache.dart';
 
-class LocalLoadSurveys {
+class LocalLoadSurveys implements LoadSurveys {
   final FetchCacheStorage fetchCacheStorage;
 
   LocalLoadSurveys({@required this.fetchCacheStorage});
