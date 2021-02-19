d38018270ba157066dbefd63c079c32beaf2af05
muller <rm80491@fiap.com.br>
2021-02-18 22:06:42 -0300

    ensure LocalStorageAdapter calls deleteItem  with correct value

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -10,6 +10,7 @@ class LocalStorageAdapter {
   LocalStorageAdapter({@required this.localStorage});
 
   Future<void> save({@required String key, @required dynamic value}) async {
+    await localStorage.deleteItem(key);
     await localStorage.setItem(key, value);
   }
 }
@@ -17,10 +18,10 @@ class LocalStorageAdapter {
 class LocalStorageSpy extends Mock implements LocalStorage {}
 
 void main() {
-  String key;
-  String value;
-  LocalStorageSpy localStorage;
   LocalStorageAdapter sut;
+  LocalStorageSpy localStorage;
+  String key;
+  dynamic value;
 
   setUp(() {
     key = faker.randomGenerator.string(5);
@@ -32,6 +33,7 @@ void main() {
   test('Should call localStorage with correct values', () async {
     await sut.save(key: key, value: value);
 
+    verify(localStorage.deleteItem(key)).called(1);
     verify(localStorage.setItem(key, value)).called(1);
   });
 }
