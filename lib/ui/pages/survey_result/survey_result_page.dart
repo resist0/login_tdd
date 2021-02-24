bd0a4024139c7d93317c537ffad4ff55f26c20a4
muller <rm80491@fiap.com.br>
2021-02-24 18:55:36 -0300

    ensure SurveyResultPage calls LoadSurveyResult on page load

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
 import '../../helpers/helpers.dart';
+import './survey_result.dart';
 
 class SurveyResultPage extends StatelessWidget {
+  final SurveyResultPresenter presenter;
+
+  SurveyResultPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,68 +15,74 @@ class SurveyResultPage extends StatelessWidget {
         title: Text(R.string.surveys),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 4,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Container(
-              padding: EdgeInsets.only(
-                top: 40,
-                left: 20,
-                right: 20,
-                bottom: 20,
-              ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor.withAlpha(90),
-              ),
-              child: Text('Qual é seu framework web favorito?'),
-            );
-          }
-          return Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).backgroundColor,
-                ),
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network(
-                      'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                      width: 40,
+      body: Builder(
+        builder: (context) {
+          presenter.loadData();
+
+          return ListView.builder(
+            itemCount: 4,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return Container(
+                  padding: EdgeInsets.only(
+                    top: 40,
+                    left: 20,
+                    right: 20,
+                    bottom: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).disabledColor.withAlpha(90),
+                  ),
+                  child: Text('Qual é seu framework web favorito?'),
+                );
+              }
+              return Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).backgroundColor,
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'Angular',
-                          style: TextStyle(fontSize: 16),
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.network(
+                          'http://fordevs.herokuapp.com/static/img/logo-angular.png',
+                          width: 40,
                         ),
-                      ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'Angular',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '100%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColorDark,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Icon(
+                            Icons.check_circle,
+                            color: Theme.of(context).highlightColor,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      '100%',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColorDark,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Icon(
-                        Icons.check_circle,
-                        color: Theme.of(context).highlightColor,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Divider(
-                height: 1,
-              ),
-            ],
+                  ),
+                  Divider(
+                    height: 1,
+                  ),
+                ],
+              );
+            },
           );
         },
       ),
---
+++ test/ui/pages/survey_result_test.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:image_test_utils/image_test_utils.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:fordev/ui/pages/pages.dart';
+
+class SurveyResultPresenterSpy extends Mock implements SurveyResultPresenter {}
+
+void main() {
+  SurveyResultPresenterSpy presenter;
+
+  void initStreams() {}
+
+  void mockStreams() {}
+
+  void closeStreams() {}
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveyResultPresenterSpy();
+    initStreams();
+    mockStreams();
+    final surveysPage = GetMaterialApp(
+      initialRoute: '/survey_result/any_survey_id',
+      getPages: [
+        GetPage(
+          name: '/survey_result/:survey_id',
+          page: () => SurveyResultPage(presenter),
+        ),
+      ],
+    );
+    provideMockedNetworkImages(() async {
+      await tester.pumpWidget(surveysPage);
+    });
+  }
+
+  List<SurveyViewModel> makeSurveys() => [
+        SurveyViewModel(
+          id: '1',
+          question: 'Question 1',
+          date: 'Date 1',
+          didAnswer: true,
+        ),
+        SurveyViewModel(
+          id: '2',
+          question: 'Question 2',
+          date: 'Date 2',
+          didAnswer: false,
+        ),
+      ];
+
+  tearDown(() {
+    closeStreams();
+  });
+
+  testWidgets('Should call LoadSurveyResult on page load',
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadData()).called(1);
+  });
+}
