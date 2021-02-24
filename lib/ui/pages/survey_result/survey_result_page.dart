32206a34e3aaa796debf93b6d10d5e31389aec43
muller <rm80491@fiap.com.br>
2021-02-24 19:57:45 -0300

    move SurveyResult components to its own file

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -3,6 +3,7 @@ import 'package:fordev/ui/components/components.dart';
 
 import '../../helpers/helpers.dart';
 import './survey_result.dart';
+import 'components/components.dart';
 
 class SurveyResultPage extends StatelessWidget {
   final SurveyResultPresenter presenter;
@@ -29,85 +30,20 @@ class SurveyResultPage extends StatelessWidget {
           presenter.loadData();
 
           return StreamBuilder<dynamic>(
-              stream: presenter.surveyResultStream,
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return ReloadScreen(
-                    error: snapshot.error,
-                    reload: presenter.loadData,
-                  );
-                }
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: 4,
-                    itemBuilder: (context, index) {
-                      if (index == 0) {
-                        return Container(
-                          padding: EdgeInsets.only(
-                            top: 40,
-                            left: 20,
-                            right: 20,
-                            bottom: 20,
-                          ),
-                          decoration: BoxDecoration(
-                            color:
-                                Theme.of(context).disabledColor.withAlpha(90),
-                          ),
-                          child: Text('Qual é seu framework web favorito?'),
-                        );
-                      }
-                      return Column(
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Theme.of(context).backgroundColor,
-                            ),
-                            padding: const EdgeInsets.all(15.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Image.network(
-                                  'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                                  width: 40,
-                                ),
-                                Expanded(
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 10),
-                                    child: Text(
-                                      'Angular',
-                                      style: TextStyle(fontSize: 16),
-                                    ),
-                                  ),
-                                ),
-                                Text(
-                                  '100%',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context).primaryColorDark,
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 10),
-                                  child: Icon(
-                                    Icons.check_circle,
-                                    color: Theme.of(context).highlightColor,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Divider(
-                            height: 1,
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                }
-                return SizedBox(height: 0);
-              });
+            stream: presenter.surveyResultStream,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return ReloadScreen(
+                  error: snapshot.error,
+                  reload: presenter.loadData,
+                );
+              }
+              if (snapshot.hasData) {
+                return SurveyResult();
+              }
+              return SizedBox(height: 0);
+            },
+          );
         },
       ),
     );
