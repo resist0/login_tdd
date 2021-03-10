343e4378b733df25abab8a357b160910552aec7f
muller <rm80491@fiap.com.br>
2021-03-10 20:10:34 -0300

    ensure Composite calls remote loadBySurvey

---
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -0,0 +1,37 @@
+import 'package:faker/faker.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:fordev/data/usecases/usecases.dart';
+
+class RemoteLoadSurveyResultWithLocalFallback {
+  final RemoteLoadSurveyResult remote;
+
+  RemoteLoadSurveyResultWithLocalFallback({@required this.remote});
+
+  Future<void> loadBySurvey({String surveyId}) async {
+    await remote.loadBySurvey(surveyId: surveyId);
+  }
+}
+
+class RemoteLoadSurveyResultSpy extends Mock implements RemoteLoadSurveyResult {
+}
+
+void main() {
+  RemoteLoadSurveyResultSpy remote;
+  RemoteLoadSurveyResultWithLocalFallback sut;
+  String surveyId;
+
+  setUp(() {
+    surveyId = faker.guid.guid();
+    remote = RemoteLoadSurveyResultSpy();
+    sut = RemoteLoadSurveyResultWithLocalFallback(remote: remote);
+  });
+
+  test('Should call remote LoadBySurvey', () async {
+    await sut.loadBySurvey(surveyId: surveyId);
+
+    verify(remote.loadBySurvey(surveyId: surveyId)).called(1);
+  });
+}
