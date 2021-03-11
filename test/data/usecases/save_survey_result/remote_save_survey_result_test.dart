9c49fed678c2fccb7db8ac4ed544c212eac4f549
muller <rm80491@fiap.com.br>
2021-03-11 20:07:21 -0300

    ensure RemoteSaveSurveyResult throws DomainError if HttpClient throws HttpErro

--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -89,6 +89,14 @@ void main() {
     ));
   });
 
+  test('Should throw UnexpectedError if HttpClient returns 200 with invalid date', () async {
+    mockHttpData({'invalid_key': 'invalid_value'});
+
+    final future = sut.save(answer: answer);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
   test('Should throw UnexpectedError if HttpClient returns 404', () async {
     mockHttpError(HttpError.notFound);
 
